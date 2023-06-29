cb9fc0adead09eb83800806320a80db5f2847fe9
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-06-29 12:22:51 +0700

    fgf

--- t.py
+++ t.py
@@ -1,8 +1,42 @@
-original_list = ['sdf', 'sdfsdf', 'sdfsdf', 'sdfsdf']
-appended_list = [list(element).append('x') for element in original_list]
+import tkinter as tk
+from tkinter import filedialog
+import subprocess
 
-arrraguments_list = []
-for x in original_list:
-    arrraguments_list.append([x])
+class AppRunner:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Script Runner")
 
-print(arrraguments_list)
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label1 = tk.Label(self.root, text="Select a Python script:")
+        self.label1.pack()
+
+        self.file_button = tk.Button(self.root, text="Browse", command=self.browse_file)
+        self.file_button.pack()
+
+        self.run_button = tk.Button(self.root, text="Run Script", command=self.run_script)
+        self.run_button.pack()
+
+    def browse_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Python Files", "*.py")])
+        self.label1.config(text="Selected Script: " + file_path)
+        self.script_path = file_path
+
+    def run_script(self):
+        if hasattr(self, 'script_path'):
+            try:
+                subprocess.Popen(['python', self.script_path])
+            except FileNotFoundError:
+                tk.messagebox.showerror("Error", "Python interpreter not found.")
+        else:
+            tk.messagebox.showerror("Error", "No script selected.")
+
+    def run(self):
+        self.root.mainloop()
+
+
+if __name__ == '__main__':
+    app = AppRunner()
+    app.run()
